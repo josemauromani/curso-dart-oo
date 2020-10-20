ba3c477358bfcb2dbacac85dfbfd858e80f4a37e
dermoagenda <josemauromani@gmail.com>
2020-10-20 17:46:54 -0300

    updates

--- bin/main.dart
+++ bin/main.dart
@@ -1,35 +1,56 @@
 void main() {
-    ContaCorrente conta1 = ContaCorrente('José Mauro', 6919, 23784,800.00);
-    conta1.saldo = 500;
-    conta1.saque(150.50);
-    print(conta1.saldo);
-    conta1.saque(400);
-    print(conta1.saldo);
+  ContaCorrente conta1 = ContaCorrente('José Mauro', 6919, 23784, 800.00);
+  conta1.saldo = 0;
+  conta1.limite = 500;
+
+  print(conta1.getSaldo());
+  print(conta1.deposito(100.50));
+  print(conta1.saque(50.50));
+  print(conta1.saque(500));
+  print(conta1.saque(60));
+  print(conta1.deposito(1000));
 }
 
 class ContaCorrente {
-    final String titular;
-    final int agencia;
-    final int conta;
-    double saldo = 0.00;
-    double limite;
+  final String titular;
+  final int agencia;
+  final int conta;
+  double saldo = 0.00;
+  double limite;
 
-    ContaCorrente(
-        this.titular,
-        this.agencia,
-        this.conta,
-        this.limite,
-    );
+  ContaCorrente(
+    this.titular,
+    this.agencia,
+    this.conta,
+    this.limite,
+  );
 
-    void saque(double valor)
-    {
-        if (this.saldo < valor) {
-            print('não e possivel sacar');
-        } else {
-            this.saldo -= valor;
-        }
+  deposito(double valor) {
+    print("DEPÓSITO = ${valor}");
+    if (valor > 0) {
+      this.saldo += valor;
     }
+    return this.getSaldo();
+  }
 
+  saque(double valor) {
+    print("SOLICITAÇÃO DE SAQUE = ${valor}");
+    if (valor > (this.saldo + this.limite)) {
+      print('saldo insuficiente');
+    } else {
+      if (valor <= this.saldo) {
+        this.saldo -= valor;
+      } else {
+        var $saqueLimite = valor - this.saldo;
+        this.saldo = 0;
+        this.limite -= $saqueLimite;
+      }
+    }
+    return this.getSaldo();
+  }
 
+  getSaldo() {
+    print("*************");
+    return "SALDO = ${this.saldo} | LIMITE = ${this.limite}";
+  }
 }
-    
